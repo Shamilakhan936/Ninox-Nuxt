da43ae7dffc5b6f6a92ea5ef60b17911674dc2ea
KeeganBeuthin <kdbeuthin@gmail.com>
2025-06-11 14:20:16 +0200

    delete irrelevant files

--- pages/orders2.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container mx-auto px-4 py-8">
-    <TestOrderForm />
-  </div>
-</template>
-
-<script setup>
-import TestOrderForm from '~/components/TestOrderForm.vue'
-</script>
